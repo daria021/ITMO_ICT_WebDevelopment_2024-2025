c761f05935dd8bb72d8671911dc9dd0e58146cb5
Дарья Едигарева <daria@Daras-MacBook-Air.local>
2024-11-13 22:26:43 +0300

    lab3 - practical work

--- students/k3339/laboratory_works/Edigareva_Daria/lab2/travel_agency/tours/urls.py
+++ students/k3339/laboratory_works/Edigareva_Daria/lab2/travel_agency/tours/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path, reverse_lazy
 from django.contrib.auth.views import LogoutView, LoginView
 from .views import SignUpView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -23,4 +26,19 @@ urlpatterns = [
     path('sold-tours/', views.sold_tours_by_country, name='sold_tours_by_country'),
 ]
 
-
+#
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="API Documentation",
+#         default_version='v1',
+#         description="Документация для Warriors API",
+#         contact=openapi.Contact(email="your-email@example.com"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
+#
+# urlpatterns += [
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+# ]
