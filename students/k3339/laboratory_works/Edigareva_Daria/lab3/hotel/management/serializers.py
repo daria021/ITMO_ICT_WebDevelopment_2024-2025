5a52f4cedb02f293dc91836081cf071322c07c90
Дарья Едигарева <daria@Daras-MacBook-Air.local>
2024-12-02 23:45:12 +0300

    initial

---
+++ students/k3339/laboratory_works/Edigareva_Daria/lab3/hotel/management/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import Room, Client, Booking, Staff, CleaningSchedule, CleaningConfirmation
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    client = serializers.PrimaryKeyRelatedField(queryset=Client.objects.all())
+    room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = '__all__'
+
+
+
+class CleaningScheduleSerializer(serializers.ModelSerializer):
+    staff = serializers.PrimaryKeyRelatedField(queryset=Staff.objects.all())
+
+    class Meta:
+        model = CleaningSchedule
+        fields = '__all__'
+
+
+
+
+class CleaningConfirmationSerializer(serializers.ModelSerializer):
+    schedule = serializers.PrimaryKeyRelatedField(queryset=CleaningSchedule.objects.all())
+    room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
+
+    class Meta:
+        model = CleaningConfirmation
+        fields = '__all__'
+
